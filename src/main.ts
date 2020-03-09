6e3062fa215143e2662189fe4dac23c2a8c0d595
Vincent Lainé <vincent.laine.utc@gmail.com>
2020-03-09 18:45:29 +0100

    Feat: ajout de Swagger / OpenApi :sparkles:

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Backoffice Chatbot')
+    .setDescription('The backoffice chatbot factory API description')
+    .setVersion('0.1')
+    .addTag('init')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
