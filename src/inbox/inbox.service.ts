e9c0ee76e68fe53fedf1ef6bff6c34fe48407e93
Vincent Lainé <vincent.laine.utc@gmail.com>
2020-07-15 15:24:41 +0200

    Fix: ratio response ok :bug:

--- src/inbox/inbox.service.ts
+++ src/inbox/inbox.service.ts
@@ -212,12 +212,12 @@ export class InboxService {
     return query.getRawOne();
   }
 
-  findRatioResponseOk(filters: StatsFilterDto): Promise<string> {
+  async findRatioResponseOk(filters: StatsFilterDto): Promise<string> {
     const startDate = filters.startDate ? moment(filters.startDate).format('YYYY-MM-DD') : null;
     const endDate = filters.endDate ? moment(filters.endDate).format('YYYY-MM-DD') : null;
 
     const query = this._inboxesRepository.createQueryBuilder('inbox')
-      .select(`(100 * (SELECT COUNT(inbox.id) from inbox WHERE confidence >= 60)/COUNT(inbox.id)) as ratioResponseOk`);
+      .select(`100 * (SELECT COUNT(inbox.id) from inbox WHERE inbox.confidence >= 0.6)/COUNT(inbox.id) as ratioResponseOk`);
     if (startDate) {
       query.where(`DATE(inbox.created_at) >= '${startDate}'`)
     }
