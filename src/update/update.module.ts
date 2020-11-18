ffbd0c2391a946c95d7783b664edc37693eb3930
Vincent Lainé <vincent.laine.utc@gmail.com>
2020-11-18 09:28:43 +0100

    Feat: fix bugs on update :sparkles:

--- src/update/update.module.ts
+++ src/update/update.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UpdateController } from './update.controller';
 import { UpdateService } from './update.service';
+import { ChatbotConfigModule } from "../chatbot-config/chatbot-config.module";
 
 @Module({
+  imports: [
+    ChatbotConfigModule
+  ],
   controllers: [UpdateController],
   providers: [UpdateService]
 })
