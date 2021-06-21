a2623b8f682dd9e767758029eb19e8d491ffe6ee
Vincent Lainé <vincent.laine.utc@gmail.com>
2021-06-21 16:41:55 +0200

    Feat: update artillery & nginx version :sparkles:

--- test/artillery/functions.js
+++ test/artillery/functions.js
@@ -6,7 +6,8 @@
 'use strict';
 
 module.exports = {
-  setMessage: setMessage
+  setMessage: setMessage,
+  getUuid: uuidv4
 };
 
 const MESSAGES = [
@@ -28,4 +29,11 @@ function setMessage(context, events, done) {
   // make it available to templates as "message"
   context.vars.message = MESSAGES[index];
   return done();
+}
+
+function uuidv4() {
+  return 'xxxxxxxx-xxxx'.replace(/[xy]/g, function (c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
 }
