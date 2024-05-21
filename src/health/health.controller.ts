544086a184752ef5a4f95efb3bb6249394927392
Octanium <octaniumcontact@gmail.com>
2024-05-21 17:05:33 +0200

    feat: change healthcheck url to env variables
    Signed-off-by: Octanium <octaniumcontact@gmail.com>

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,10 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import {
-  HttpHealthIndicator,
   HealthCheck,
   HealthCheckService,
+  HttpHealthIndicator,
 } from '@nestjs/terminus';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('health')
 @Controller('health')
@@ -19,8 +19,8 @@ export default class HealthController {
   @ApiOperation({ summary: 'Etat de santé du serveur' })
   check() {
     return this.health.check([
-      () => this.dns.pingCheck('backoffice', 'http://127.0.0.1'),
-      () => this.dns.pingCheck('rasa', 'http://127.0.0.1:5005'),
+      () => this.dns.pingCheck('backoffice', process.env.HOST_URL),
+      () => this.dns.pingCheck('rasa', process.env.RASA_API),
     ]);
   }
 }
