548bb1d8d468d865b364eef96da7aaaa86da55b7
Octanium <octaniumcontact@gmail.com>
2024-05-21 17:05:02 +0200

    feat: add status code and response time to response log
    Signed-off-by: Octanium <octaniumcontact@gmail.com>

--- src/core/interceptors/logger.interceptor.ts
+++ src/core/interceptors/logger.interceptor.ts
@@ -1,14 +1,27 @@
 import {
+  CallHandler,
+  ExecutionContext,
   Injectable,
   NestInterceptor,
-  ExecutionContext,
-  CallHandler,
 } from '@nestjs/common';
+import { randomBytes } from 'node:crypto';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import BotLogger from '../../logger/bot.logger';
 
-const crypto = require('crypto');
+function getResponseTime(requestAt, responseAt, digits) { 
+  if (!requestAt || !responseAt) {
+    // missing request and/or response start time
+    return 'n/a'
+  }
+
+  // calculate diff
+  const ms = (responseAt[0] - requestAt[0]) * 1e3 +
+    (responseAt[1] - requestAt[1]) * 1e-6
+
+  // return truncated value
+  return ms.toFixed(digits === undefined ? 3 : digits)
+}
 
 @Injectable()
 export default class LoggerInterceptor implements NestInterceptor {
@@ -20,21 +33,28 @@ export default class LoggerInterceptor implements NestInterceptor {
    * @param next
    */
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    const id = crypto.randomBytes(8).toString('hex');
-    const req = context.getArgByIndex(0);
+    const id = randomBytes(8).toString('hex');
+    const ctx = context.switchToHttp();
+    const req = ctx.getRequest()
+    const res = ctx.getResponse()
+
     const body = Buffer.isBuffer(req.body) ? null : { ...req.body };
     if (body?.password) {
       body.password = 'PASSWORD';
     }
     this.logger.log(
-      `REQUEST - ${id} - ${req.ip} - ${req.user?.email} - ${req.method} - ${
+      `REQUEST - ${id} - ${req.ip} - ${req.user?.email ?? 'Anonymous'} - ${req.method} - ${
         req.originalUrl
       } - ${!body ? '' : JSON.stringify(body)}`,
     );
 
+    const requestAt = process.hrtime()
+
     return next.handle().pipe(
       tap(() => {
-        this.logger.log(`RESPONSE - ${id}`);
+        const responseAt = process.hrtime()
+
+        this.logger.log(`RESPONSE - ${id} - ${res.statusCode} - ${getResponseTime(requestAt, responseAt, 3)}ms`);
       }),
     );
   }
